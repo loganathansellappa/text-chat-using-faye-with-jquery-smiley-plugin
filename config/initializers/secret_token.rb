df99ab5b62352ca31a878390e9d791d7e6385b50
Logan <loganathan.ms@gmail.com>
2013-01-01 19:28:12 +0530

    Chat functionality implemented

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TextChatUsingFaye::Application.config.secret_token = '5b4e82c205526e720c49f9ecddc65f2f52490957fec3f05b0e84978d903bd34095bff598ed96614673530a4405ea03e714de9c5c0ed1c0e05b5aca870499a53a'
